15b165cda2c858ed3453b18aec0de314e2ef6cbd
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-12-08 22:33:22 +0630

    Form Validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -21,12 +21,27 @@ export const YouTubeForm = () => {
   return (
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: "Username is required",
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
