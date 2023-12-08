b66ef2868f188b38d6d773e21c8cf5303afb9137
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-12-08 22:11:47 +0630

    Form State and Rerenders

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,12 +1,17 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
+
 export const YouTubeForm = () => {
   const form = useForm();
   const { register, control } = form;
 
+  renderCount++;
+
   return (
     <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
       <form>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
