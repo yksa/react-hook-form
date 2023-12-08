e23d63690acc80afe665de565de011084b660445
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-12-08 21:47:18 +0630

    Managin Form State 1

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,22 +1,31 @@
-import { useForm } from 'react-hook-form'
+import { useForm } from "react-hook-form";
 
 export const YouTubeForm = () => {
-  const form = useForm()
+  const form = useForm();
+  const { register } = form;
+  const { name, ref, onChange, onBlur } = register("username");
 
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id='username' name='username' />
+        <input
+          type="text"
+          id="username"
+          name={name}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id='email' name='email' />
+        <input type="email" id="email" name="email" />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id='channel' name='channel' />
+        <input type="text" id="channel" name="channel" />
 
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
